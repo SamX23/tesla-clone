3e7deb0e950512c57c3330a2b847ba27d7759443
SamX23 <skalexsong@gmail.com>
2022-01-03 21:34:06 +0700

    Update : create custom meta tag element

---
+++ components/Meta.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+    <Head>
+      <title>Electric Cars, Solar & Clean Energy | Tesla</title>
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    </Head>
+  );
+};
+
+export default Meta;
