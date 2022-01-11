f9afc7a87b5c4a08769a719cd15b9bbd37db8bd7
SamX23 <skalexsong@gmail.com>
2022-01-11 22:12:00 +0700

    Update : add OG meta tag

--- components/Meta.js
+++ components/Meta.js
@@ -3,7 +3,34 @@ import Head from "next/head";
 const Meta = () => {
   return (
     <Head>
-      <title>Electric Cars, Solar & Clean Energy | Tesla</title>
+      <title>Electric Cars, Solar & Clean Energy | Tesla Clone</title>
+      <meta
+        name="title"
+        content="Electric Cars, Solar & Clean Energy | Tesla Clone"
+      />
+      <meta
+        name="description"
+        content="A clone of tesla homepage created using Next JS only for learning purpose, created by : Sami Kalammallah"
+      />
+      <meta
+        property="og:title"
+        content="Electric Cars, Solar & Clean Energy | Tesla Clone"
+      />
+      <meta property="og:site_name" content="Tesla Landing Page Clone" />
+      <meta property="og:url" content="https://tesla-clone-black.vercel.app/" />
+      <meta
+        property="og:description"
+        content="A clone of tesla homepage created using Next JS only for learning purpose, created by : Sami Kalammallah"
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://tesla-clone-black.vercel.app/Assets/home-1.webp"
+      />
+      <meta
+        name="keywords"
+        content="tesla, tesla clone, sami kalammallah, landing page"
+      />
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     </Head>
   );
